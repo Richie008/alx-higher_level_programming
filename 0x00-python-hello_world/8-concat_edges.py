384ac9beaa70d0f351b1085793f0591481097f26
richie008 <richardabia74@gmail.com>
2023-06-05 08:28:40 -0700

    hey pyhton

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/bin/bash
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"{str[39:-62]}with python\n")
---
+++ 0x00-python-hello_world/main2.py
@@ -0,0 +1,1 @@
+print("Alx is fun")
