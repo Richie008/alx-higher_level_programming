29f56f8300694e884e72c4235fdea02296c3752c
richie008 <richardabia74@gmail.com>
2023-06-06 00:15:34 -0700

    hey pyhton

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,5 @@
-#!/bin/bash
+#!/usr/bin/python3
 import sys
-
-message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-sys.stderr.write(message)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
 
